7ab51777505907d79effd47658413c0b0653d22a
Karki <pkarki10@LAMU02FN5ZCMD6R.uhc.com>
2022-03-25 23:10:38 +0530

    created jar for docker

--- src/main/java/com/finalproject/medicare/controllers/LoginController.java
+++ src/main/java/com/finalproject/medicare/controllers/LoginController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/medicare")
 public class LoginController {
